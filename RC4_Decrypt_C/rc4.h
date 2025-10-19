e118216bb5aa4e82e06884b40d7d3ec4efe3c5d3
ThThHuong <aromarisstore@gmail.com>
2025-10-19 19:47:11 +0700

    rc4.h

---
+++ RC4_Decrypt_C/rc4.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <algorithm>
+
+class RC4 {
+private:
+    unsigned char S[256];
+public:
+    RC4(unsigned char* key, int keylen);
+    void crypt(unsigned char* data, int len);
+};
